345659e5d6c674d70e924fbc62e7f085db61d5ee
David Olorundare <crystilis@gmail.com>
2018-11-06 16:02:53 -0600

    Change file indentation of java file

--- src/main/java/io/jenkins/plugins/audit/UserCreationListener.java
+++ src/main/java/io/jenkins/plugins/audit/UserCreationListener.java
@@ -1,56 +1,56 @@
-  /*
-   * The MIT License
-   *
-   * Permission is hereby granted, free of charge, to any person obtaining a copy
-   * of this software and associated documentation files (the "Software"), to deal
-   * in the Software without restriction, including without limitation the rights
-   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   * copies of the Software, and to permit persons to whom the Software is
-   * furnished to do so, subject to the following conditions:
-   *
-   * The above copyright notice and this permission notice shall be included in
-   * all copies or substantial portions of the Software.
-   *
-   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-   * THE SOFTWARE.
-   */
+    /*
+     * The MIT License
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy
+     * of this software and associated documentation files (the "Software"), to deal
+     * in the Software without restriction, including without limitation the rights
+     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the Software is
+     * furnished to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included in
+     * all copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     * THE SOFTWARE.
+     */
 
-   package io.jenkins.plugins.audit;
+    package io.jenkins.plugins.audit;
 
-   import hudson.ExtensionList;
-   import hudson.ExtensionPoint;
+    import hudson.ExtensionList;
+    import hudson.ExtensionPoint;
 
-   import java.net.InetAddress;
-   import java.time.Instant;
+    import java.net.InetAddress;
+    import java.time.Instant;
 
-   import org.apache.logging.log4j.audit.event.CreateUser;
-   import org.apache.logging.log4j.audit.LogEventFactory;
-   import org.apache.logging.log4j.core.util.NetUtils;
+    import org.apache.logging.log4j.audit.event.CreateUser;
+    import org.apache.logging.log4j.audit.LogEventFactory;
+    import org.apache.logging.log4j.core.util.NetUtils;
 
-   /**
-    * Listener notified of user account creation events.
-    */
+    /**
+     * Listener notified of user account creation events.
+     */
     public abstract class UserCreationListener extends ExtensionPoint {
-
+      
         CreateUser user = LogEventFactory.getEvent(CreateUser.class);
         String hostName = NetUtils.getLocalHostname();
         String inetAddress = InetAddress.getLocalHost().getHostAddress();
 
-       /**
-        * Fired when a user account is created.
-        * @param username the user
-        */
+        /**
+         * Fired when a user account is created.
+         * @param username the user
+         */
         protected void userCreated(@Nonnull String username) {}
 
-       /**
-        * Fired when a user account is created, notifies all listeners.
-        * @param username the user
-        */
+        /**
+         * Fired when a user account is created, notifies all listeners.
+         * @param username the user
+         */
         public static void fireUserCreated(@Nonnull String username) {
             String currentTime = Instant.now().toString();
             RequestContext.setHostName(hostName);
@@ -72,4 +72,5 @@
             return ExtensionList.lookup(UserCreationListener.class);
         }
 
+      
     }
