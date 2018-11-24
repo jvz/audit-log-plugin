f4762987a837beb9a3419de2f7757c713fa31e55
David Olorundare <david.o@ieee.org>
2018-11-23 20:48:00 -0600

    "[HOSTING-670] Refactor pom.xml
    The <artifactId> from the pom.xml is incorrect,
    and should be the new repository name with -plugin
    removed.
    "

--- src/main/java/io/jenkins/plugins/audit/listeners/UserLogListener.java
+++ src/main/java/io/jenkins/plugins/audit/listeners/UserLogListener.java
@@ -109,4 +109,6 @@ public class UserLogListener extends SecurityListener {
       public static ExtensionList<UserLogListener> all() {
           return ExtensionList.lookup(UserLogListener.class);
       }
+
+
 }
